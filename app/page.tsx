22c823831b7dd1e75cf55d55d7b3ec5beee3b127
Gustavo Tempone de Oliveira Faria <gustavo.faria@efrete.com.br>
2025-03-26 12:22:55 -0300

    fix: order responsivity for illustration

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
         <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container px-4 md:px-6">
             <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 xl:grid-cols-2">
-              <div className="flex flex-col justify-center space-y-4">
+              <div className="flex flex-col order-2 justify-center space-y-4 md:order-1">
                 <div className="space-y-2">
                   <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                     Aprenda a usar a IA para automatizar seu trabalho
@@ -98,7 +98,8 @@ export default function Home() {
                   </span>
                 </div>
               </div>
-              <div className="flex items-center justify-center">
+
+              <div className="flex items-center justify-center order-1">
                 <img
                   src={HomeImage.src}
                   alt="Home"
