5073eb0f54b44beb1bc8fb6d062efc840f9017f0
Gustavo Tempone de Oliveira Faria <gustavo.faria@nstechfin.com.br>
2025-07-14 07:51:46 -0300

    fix: async await project and package versions

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,8 +5,13 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import { Fragment } from "react";
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
-  const project = getProjectBySlug(params.slug);
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+  const project = getProjectBySlug(slug);
 
   if (!project) {
     notFound();
